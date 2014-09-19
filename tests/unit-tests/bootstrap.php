433b1c0be36906103127483063b21fa0e736394a
nsmn38 <niklas.simonis@mni.thm.de>
2014-09-19 13:43:51 +0200

    *change bootstrap

--- tests/unit-tests/bootstrap.php
+++ tests/unit-tests/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 if (!defined('JPATH_TESTS'))
 {
-    define('JPATH_TESTS', dirname(__FILE__));
+	define('JPATH_TESTS', realpath(dirname(__DIR__)));
 }
-
-// require_once dirname(dirname(__FILE__)) . '/bootstrapJ3.php';
+if (!defined('JPATH_BASE'))
+{
+	define('JPATH_BASE', realpath(dirname(dirname(dirname(__DIR__)))));
+}
 
 // Import the Joomla bootstrap.
 require_once JPATH_BASE . '/tests/bootstrapJ3.php';
