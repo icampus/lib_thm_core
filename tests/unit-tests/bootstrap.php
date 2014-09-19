c228a0df8f091065f1ac94c1a401055892bf7bdb
nsmn38 <niklas.simonis@mni.thm.de>
2014-09-19 13:13:54 +0200

    *fix bootstrap

--- tests/unit-tests/bootstrap.php
+++ tests/unit-tests/bootstrap.php
@@ -4,4 +4,7 @@ if (!defined('JPATH_TESTS'))
     define('JPATH_TESTS', dirname(__FILE__));
 }
 
-require_once dirname(dirname(__FILE__)) . '/bootstrapJ3.php';
+// require_once dirname(dirname(__FILE__)) . '/bootstrapJ3.php';
+
+// Import the Joomla bootstrap.
+require_once JPATH_BASE . '/tests/bootstrapJ3.php';
