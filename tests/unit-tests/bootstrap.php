1cb828b55fedaf050b44b155102fd16a2fad3093
nsmn38 <niklas.simonis@mni.thm.de>
2014-09-19 13:50:08 +0200

    *fix bootstrap

--- tests/unit-tests/bootstrap.php
+++ tests/unit-tests/bootstrap.php
@@ -7,6 +7,14 @@ if (!defined('JPATH_BASE'))
 {
 	define('JPATH_BASE', realpath(dirname(dirname(dirname(__DIR__)))));
 }
+if (!defined('JPATH_PLATFORM'))
+{
+    define('JPATH_PLATFORM', JPATH_BASE . '/libraries');
+}
+if (!defined('JPATH_LIBRARIES'))
+{
+    define('JPATH_LIBRARIES', JPATH_BASE . '/libraries');
+}
 
 // Import the Joomla bootstrap.
 require_once JPATH_BASE . '/tests/bootstrapJ3.php';
