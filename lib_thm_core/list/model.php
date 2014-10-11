b9e603879f0c5c2bc9675716d225d8900ee33ece
James Antrim <james.antrim@mni.thm.de>
2014-10-11 12:51:44 +0200

    changed 2012 copyright to 21014 added protected default class values which allow view to set their own default ordering and direction in the common model file they inherit from

--- lib_thm_core/list/model.php
+++ lib_thm_core/list/model.php
@@ -3,10 +3,10 @@
  * @category    Joomla library
  * @package     THM_Core
  * @subpackage  lib_thm_core.site
- * @name        THM_CoreListTemplate
+ * @name        THM_CoreListModel
  * @description Common template for list views
- * @author      Melih Cakir, <melih.cakir@mni.thm.de>
  * @author      James Antrim, <james.antrim@mni.thm.de>
+ * @author      Ilja Michajlow, <Ilja.Michajlow@mni.thm.de>
  * @copyright   2014 TH Mittelhessen
  * @license     GNU GPL v.2
  * @link        www.mni.thm.de
@@ -21,6 +21,9 @@
  */
 class THM_CoreListModel extends JModelList
 {
+    protected $defaultOrdering = '';
+
+    protected $defaultDirection = '';
 
     /**
      * Overwrites the JModelList populateState function
@@ -32,15 +35,66 @@ class THM_CoreListModel extends JModelList
     {
         $app = JFactory::getApplication();
 
-        $search = $app->getUserStateFromRequest($this->context . 'filter.search', 'filter_search', '');
-        $this->state->set('filter.search', $search);
+        // Receive & set filters
+        $filters = $app->getUserStateFromRequest($this->context . '.filter', 'filter', array(), 'array');
+        if (!empty($filters))
+        {
+            foreach ($filters as $name => $value)
+            {
+                $this->setState('filter.' . $name, $value);
+            }
+        }
 
-        $ordering = $app->getUserStateFromRequest($this->context . '.filter_order', 'filter_order', '');
-        $this->state->set('list.ordering', $ordering);
+        $orderingSet = $this->processFullOrdering();
+        if (!$orderingSet)
+        {
+            $ordering = $app->getUserStateFromRequest($this->context . '.list.order', 'list.order', $this->defaultOrdering);
+            $this->state->set('list.ordering', $ordering);
 
-        $direction = $app->getUserStateFromRequest($this->context . 'filter.direction', 'filter_direction', '');
-        $this->state->set('list.direction', $direction);
+            $direction = $app->getUserStateFromRequest($this->context . 'list.direction', 'list.direction', $this->defaultDirection);
+            $this->state->set('list.direction', $direction);
+        }
 
-        parent::populateState($ordering, $direction);
+
+        $limit = $app->getUserStateFromRequest($this->context . 'list.limit', 'list.limit', '20');
+        $this->state->set('list.limit', $limit);
+
+        $start = $app->getUserStateFromRequest($this->context . 'list.limitstart', 'list.limitstart', '0');
+        $this->state->set('list.start', $start);
+    }
+
+    /**
+     * Sets the ordering and direction filters should a valid full ordering request be made
+     *
+     * @return  bool  true if the full ordering exists and is of correct syntax, otherwise false
+     */
+    private function processFullOrdering()
+    {
+        $fullOrdering = JFactory::getApplication()->getUserStateFromRequest($this->context . '.list.fullordering', 'list.fullordering', '');
+
+        // Not set
+        if (empty($fullOrdering))
+        {
+            return false;
+        }
+
+        $orderingParts = explode($fullOrdering, ' ');
+
+        // Invalid number of arguments
+        if (count($orderingParts) != 2)
+        {
+            return false;
+        }
+
+        // Valid entry
+        if (in_array(strtoupper($fullOrdering[1]), array('ASC', 'DESC', '')))
+        {
+            $this->setState('list.ordering', $fullOrdering[0]);
+            $this->setState('list.direction', $fullOrdering[1]);
+            return true;
+        }
+
+        // Invalid direction
+        return false;
     }
 }
