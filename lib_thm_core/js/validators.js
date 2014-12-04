f3b1dd5bbd1c9a90b1df19fcd46c82aa26ac9186
James Antrim <james.antrim@mni.thm.de>
2014-12-04 10:16:20 +0100

    changes which were not integrated because they were external
    Change-Id: Icaf0929d61e473a6a8940298a134015e1303b441

---
+++ lib_thm_core/js/validators.js
@@ -0,0 +1,13 @@
+/**
+ * Contains form validation expressions
+ */
+
+window.addEvent('domready', function()
+{
+    "use strict";
+    document.formvalidator.setHandler('ip',
+        function (value)
+        {
+            return (/^[0-2]*[0-9]*[0-9].[0-2]*[0-9]*[0-9].[0-2]*[0-9]*[0-9].[0-2]*[0-9]*[0-9]$/).test(value);
+        });
+});
