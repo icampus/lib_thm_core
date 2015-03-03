d08d2f25a241fc705539aab71cd7f660b5b03642
Alexander Boll <alexander.boll@mni.thm.de>
2015-03-03 11:21:25 +0100

    fixed mootools extjs conflict
    Change-Id: Ic60d2339aee3c1d5a4d1d6c7ce6a8fd1eb08e151

--- lib_thm_core/js/extjs/resolveDocument.js
+++ lib_thm_core/js/extjs/resolveDocument.js
@@ -1,5 +1,7 @@
-document.id = document.$id;
-delete document.$id;
+Ext.onReady(function() {
+    document.id = document.$id;
+    delete document.$id;
+});
 
 // Workaround for IE 11 (which is not supported in Ext JS 4.0.7)
 Ext.apply(Ext, {
