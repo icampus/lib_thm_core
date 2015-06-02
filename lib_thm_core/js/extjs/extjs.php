1544452c6900fc29a1677347c3f361b30e32527a
James Antrim <james.antrim@ft.thm.de>
2015-06-02 17:17:18 +0200

    removed unnecessary inclusions
    Change-Id: Ibc9e3852f9b96eef86bb3c7c0f672e5c1368a6b3

--- lib_thm_core/js/extjs/extjs.php
+++ lib_thm_core/js/extjs/extjs.php
@@ -1,17 +1,15 @@
 <?php
 defined('_JEXEC') or die;
 $extScripts = array();
-$extScripts[] = JURI::root()."libraries/thm_core/js/extjs/setMootoolsDoc.js";
 //if (JDEBUG)
 //{
-//    $extScripts[] = JURI::root()."libraries/extjs4/extjs/ext-all-dev.js";
+//    $extScripts[] = JURI::root()."libraries/extjs4/extjs/ext-all-debug.js";
 //}
 //else
 //{
 //    $extScripts[] = JURI::root()."libraries/extjs4/extjs/ext-all.min.js";
 //}
 $extScripts[] = JURI::root()."libraries/thm_core/js/extjs/ext-all-debug.js";
-$extScripts[] = JURI::root()."libraries/thm_core/js/extjs/resolveDocument.js";
 $doc = JFactory::getDocument();
 $scripts = array_keys($doc->_scripts);
 foreach ($extScripts as $script)
