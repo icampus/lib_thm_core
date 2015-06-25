bf16819959e5b14021f3a3aeb896f66391f7c1cc
Wolf Rost <wolf.normann.gordian.rost@mni.thm.de>
2015-06-25 14:41:08 +0200

    -fixed document.id error
    Change-Id: I38eb17e5a71c1ce46f502af8c4b8a6a80b3c04a0

--- lib_thm_core/js/extjs/extjs.php
+++ lib_thm_core/js/extjs/extjs.php
@@ -10,6 +10,8 @@ $extScripts = array();
 //    $extScripts[] = JURI::root()."libraries/extjs4/extjs/ext-all.min.js";
 //}
 $extScripts[] = JURI::root()."libraries/thm_core/js/extjs/ext-all-debug.js";
+$extScripts[] = JURI::root()."libraries/thm_core/js/extjs/setMootoolsDoc.js";
+$extScripts[] = JURI::root()."libraries/thm_core/js/extjs/resolveDocument.js";
 $doc = JFactory::getDocument();
 $scripts = array_keys($doc->_scripts);
 foreach ($extScripts as $script)
