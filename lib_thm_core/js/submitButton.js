cf366ef681e1cc0705fef6ab12768c59432abc87
Dominik Bassing <dominik.bassing@mni.thm.de>
2015-08-04 16:42:44 +0200

    Fix for submitbuttons
    Change-Id: I40bb7b4853e37ca23e6fa371c5a9d73e11e02bae

--- lib_thm_core/js/submitButton.js
+++ lib_thm_core/js/submitButton.js
@@ -1,11 +1,16 @@
 /**
  * Contains the normal form function that is executed when a form is submitted
  */
-jQuery( document ).ready(function() {
-    Joomla.submitbutton = function (task) {
-        var match = task.match(/\.cancel$/);
-        if (match !== null || document.formvalidator.isValid(document.id('item-form'))) {
-            Joomla.submitform(task, document.getElementById('item-form'));
+jQuery( document ).ready(function()
+{
+    Joomla.submitbutton = function (task)
+    {
+        var match = task.match(/\.cancel$/), form;
+        if (match !== null || document.formvalidator.isValid(document.id('item-form')))
+        {
+            form = document.getElementById('item-form');
+            form.task.value = task;
+            form.submit();
         }
     }
 });
